fd7672b946a14d7e9abdda6b065d04b5295fd1ea
Jojhan Silva <silvajojhan@gmail.com>
2023-04-12 20:24:33 -0500

    can now get pokemon type thru HTML

--- assets/script.js
+++ assets/script.js
@@ -8,35 +8,53 @@ fetch('http://api.weatherstack.com/current') {
     & units = 'f'
     & language = 'en'
 }
-// Gets all water type pokemon
-fetch('https://pokeapi.co/api/v2/type/water', {
-})
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    var waterTypePokemons = data.pokemon.map(pokemon => pokemon.pokemon.name);
-    console.log(waterTypePokemons);
-  });
 
-// Gets all fire type pokemon
-  fetch('https://pokeapi.co/api/v2/type/fire', {
-})
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    var fireTypePokemons = data.pokemon.map(pokemon => pokemon.pokemon.name);
-    console.log(fireTypePokemons);
-  });
+document.getElementById('cityButton').addEventListener('click', function() {
+  var pokemonType = document.getElementById('city').value;
+  fetchPokemonsByType(pokemonType);
+});
 
-//Gets all grass type pokemon
-fetch('https://pokeapi.co/api/v2/type/grass', {
-})
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    var grassTypePokemons = data.pokemon.map(pokemon => pokemon.pokemon.name);
-    console.log(grassTypePokemons);
-  });
+
+function fetchPokemonsByType(type) {
+  fetch(`https://pokeapi.co/api/v2/type/${type}`, {})
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var pokemons = data.pokemon.map(pokemon => pokemon.pokemon.name);
+      console.log(pokemons);
+    });
+}
+
+// // Gets all water type pokemon
+// fetch('https://pokeapi.co/api/v2/type/water', {
+// })
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     var waterTypePokemons = data.pokemon.map(pokemon => pokemon.pokemon.name);
+//     console.log(waterTypePokemons);
+//   });
+
+// // Gets all fire type pokemon
+//   fetch('https://pokeapi.co/api/v2/type/fire', {
+// })
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     var fireTypePokemons = data.pokemon.map(pokemon => pokemon.pokemon.name);
+//     console.log(fireTypePokemons);
+//   });
+
+// //Gets all grass type pokemon
+// fetch('https://pokeapi.co/api/v2/type/grass', {
+// })
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     var grassTypePokemons = data.pokemon.map(pokemon => pokemon.pokemon.name);
+//     console.log(grassTypePokemons);
+//   });
