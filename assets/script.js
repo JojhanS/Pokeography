fd87653dd15264a85332ceeadf482bc642971e4a
Jojhan Silva <silvajojhan@gmail.com>
2023-04-13 20:38:10 -0500

    cleaned up commented out code

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,6 @@
 // Weather API
-var locationString = ""
-// var currentWeather = [
-//     currentTime = ""
-//     temperature = ""
-//     weatherCode = ""
-//     windSpeed = ""
-// ]
+var locationString = "";
+
 document.getElementById('cityButton').addEventListener('click', function(event){
     event.preventDefault();
     locationString = document.getElementById('city').value;
@@ -19,38 +14,10 @@ fetch("https://chriscastle.com/proxy/?:proxy:http://api.weatherstack.com/current
 })
     .then(function(data) {
         console.log(data)
-        // var currentWeather = data.current
 })
 }
 
-// function locationInput() {
-//     document.getElementById('city').addEventListener('click', function(e)
-// )}
 
-// function displayWeather() {
-//     document.getElementById('weather').append(data)
-// }
-
-
-// // fetch("http://api.weatherstack.com/current?access_key=4f8d7a4bb61c72c9b79facd1733456d1&query="+location+"&units=f")
-// fetch("http://api.weatherstack.com/current?access_key=2efa48685540853e56dac3d000771904&query="+location+"&units=f")
-// .then(function (response) {
-//     console.log("hi");
-//     return response.json();
-// })
-//     .then(function(data) {
-//         console.log(data)
-//         // var currentWeather = data.current
-// })
-
-// function locationInput() {
-//    var locationString = document.getElementById('city').addEventListener('click', function(e))
-// }
-//    });
-// }
-// function displayWeather() {
-//     currentWeather = ""
-// }
 
 // Gets all water type pokemon
 fetch('https://pokeapi.co/api/v2/type/water', {
@@ -63,12 +30,6 @@ fetch('https://pokeapi.co/api/v2/type/water', {
     console.log(waterTypePokemons);
   });
 
-// document.getElementById('cityButton').addEventListener('click', function() {
-//   var pokemonType = document.getElementById('city').value;
-//   fetchPokemonsByType(pokemonType);
-// });
-
-
 function fetchPokemonsByType(type) {
   fetch(`https://pokeapi.co/api/v2/type/${type}`, {})
     .then(function (response) {
@@ -80,35 +41,3 @@ function fetchPokemonsByType(type) {
     });
 }
 
-// // Gets all water type pokemon
-// fetch('https://pokeapi.co/api/v2/type/water', {
-// })
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     var waterTypePokemons = data.pokemon.map(pokemon => pokemon.pokemon.name);
-//     console.log(waterTypePokemons);
-//   });
-
-// // Gets all fire type pokemon
-//   fetch('https://pokeapi.co/api/v2/type/fire', {
-// })
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     var fireTypePokemons = data.pokemon.map(pokemon => pokemon.pokemon.name);
-//     console.log(fireTypePokemons);
-//   });
-
-// //Gets all grass type pokemon
-// fetch('https://pokeapi.co/api/v2/type/grass', {
-// })
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     var grassTypePokemons = data.pokemon.map(pokemon => pokemon.pokemon.name);
-//     console.log(grassTypePokemons);
-//   });
