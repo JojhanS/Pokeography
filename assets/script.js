4a36f68dbe1a179d7fad3162669a8fb04b4db3d8
TCole44 <marcustcoleman4@gmail.com>
2023-04-13 23:04:47 -0400

    updated js

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 // Weather API
 var locationString = "";
-
+var weatherContainer = document.getElementById("weather");
 document.getElementById('cityButton').addEventListener('click', function(event){
     event.preventDefault();
     locationString = document.getElementById('city').value;
@@ -15,6 +15,7 @@ fetch("https://chriscastle.com/proxy/?:proxy:http://api.weatherstack.com/current
 })
     .then(function(data) {
         var getWeatherCode = data.current.weather_code
+        var getWeatherDesc = data.current.weather_descriptions
         var getWindSpeed = data.current.wind_speed
         var getTemp = data.current.temperature
         var isDay = data.current.is_day
@@ -22,24 +23,20 @@ fetch("https://chriscastle.com/proxy/?:proxy:http://api.weatherstack.com/current
         console.log(getTemp)
         console.log(isDay)
         console.log(getWeatherCode)
+        console.log(getWeatherDesc)
 
+        if (getWeatherCode > 310) {
+            fetchPokemonsByType("ice")
+        }
+        else {
+            fetchPokemonsByType("normal")
+        }
+         
 })
 }
 
 
-
-
-// Gets all water type pokemon
-fetch('https://pokeapi.co/api/v2/type/water', {
-})
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    var waterTypePokemons = data.pokemon.map(pokemon => pokemon.pokemon.name);
-    console.log(waterTypePokemons);
-  });
-
+// fetches pokemon
 function fetchPokemonsByType(type) {
   fetch(`https://pokeapi.co/api/v2/type/${type}`, {})
     .then(function (response) {
